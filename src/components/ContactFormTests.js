40a670343c47acd8019fa4171d0ce71f993e24e0
cknoettg <cknoettg@yahoo.com>
2020-05-10 20:53:56 -0400

    Initial rendering test and testing first name

--- src/components/ContactFormTests.js
+++ src/components/ContactFormTests.js
@@ -2,3 +2,12 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
+test('rendering form test...', () =>{
+    render(<ContactForm />)
+});
+
+test('testing form...', () =>{
+    const { getByLabelText } = render(<ContactForm />)
+
+    const firstName = getByLabelText(/first name/i);
+});
